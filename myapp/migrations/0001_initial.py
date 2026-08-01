25615cc55327b447b68bbf1840a50358442e24da
Gokul MS <gokulms872@gmail.com>
2026-08-01 19:42:37 +0530

    Fix custom user migration dependency

--- myapp/migrations/0001_initial.py
+++ myapp/migrations/0001_initial.py
@@ -13,7 +13,7 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        ('auth', '0013_alter_user_username'),
+         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
     operations = [
